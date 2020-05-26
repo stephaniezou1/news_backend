2da5bffaff0865e4a1786dc3a2b288ccd38dec4a
isabel k. lee <hello@isabelklee.co>
2020-05-26 17:07:22 -0400

    added show action for articles controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,4 +3,18 @@ class ArticlesController < ApplicationController
       articles = Article.all
       render json: articles
   end
+
+  def show 
+    find_article
+    render json: article
+  end
+
+  private
+  def find_article
+    article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:content, :article)
+  end
 end
