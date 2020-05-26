b1e75dfe868a094c6d7e8246ce3656067305871d
isabel k. lee <hello@isabelklee.co>
2020-05-26 16:17:03 -0400

    added joiner seed data + joiner controlller actions

--- app/serializers/tag_serializer.rb
+++ app/serializers/tag_serializer.rb
@@ -1,4 +1,5 @@
 class TagSerializer < ActiveModel::Serializer
   attributes :id, :content, :article_id
   has_many :articles
+  has_many :joiners
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,15 @@
+Joiner.destroy_all
 Article.destroy_all
 Article.get_top_headlines
 Tag.destroy_all
-Joiner.destroy_all
 
 tag1 = Tag.create(content: "dogs")
+tag2 = Tag.create(content: "politics")
+
 Joiner.create(article_id: Article.all.sample.id, tag_id: tag1.id)
+Joiner.create(article_id: Article.all.sample.id, tag_id: tag2.id)
+
+Joiner.create(article_id: Article.all.sample.id, tag_id: tag1.id)
+Joiner.create(article_id: Article.all.sample.id, tag_id: tag2.id)
 
 puts "seeding done"
