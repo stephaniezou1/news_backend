00f26aa8d496e104d4c38f51165f5a5e57ff5ae1
StephanieZou1 <stephanie.zou3@gmail.com>
2020-05-29 10:05:22 -0400

    serializer

--- app/serializers/article_serializer.rb
+++ app/serializers/article_serializer.rb
@@ -1,5 +1,5 @@
 class ArticleSerializer < ActiveModel::Serializer
-  attributes :id, :title, :author, :description, :source_name, :published_at, :url, :url_to_image, :content
+  attributes :id, :title, :author, :description, :source_name, :published_at, :url, :url_to_image, :content, :likes
   # has_many :tags
   has_many :joiners
 end
