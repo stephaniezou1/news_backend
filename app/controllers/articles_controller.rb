ec538bb5e24c33e2de1bf1f0f6eba6b38f46e713
StephanieZou1 <stephanie.zou3@gmail.com>
2020-05-29 10:41:33 -0400

    likes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,20 +7,20 @@ class ArticlesController < ApplicationController
   end
 
   def show 
-    render json: article
+    render json: @article
   end
 
   def update
-    article.update(article_params)
-    render json: article
+    @article.update(article_params)
+    render json: @article
   end
 
   private
   def find_article
-    article = Article.find(params[:id])
+    @article = Article.find(params[:id])
   end
 
   def article_params
-    params.require(:article).permit(:content, :article)
+    params.require(:article).permit(:content, :article, :likes)
   end
 end
