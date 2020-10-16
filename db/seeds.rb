ffabf3794df6f414f6efdf0549db625da499bfd5
isabel k. lee <hello@isabelklee.co>
2020-10-16 15:20:17 -0400

    removed dogs tag

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,15 +3,11 @@ Article.destroy_all
 Article.get_top_headlines
 Tag.destroy_all
 
-tag1 = Tag.create(content: "dogs")
 tag2 = Tag.create(content: "politics")
 tag3 = Tag.create(content: "environment")
 tag4 = Tag.create(content: "travel")
 
-Joiner.create(article_id: Article.all.sample.id, tag_id: tag1.id)
 Joiner.create(article_id: Article.all.sample.id, tag_id: tag2.id)
-
-Joiner.create(article_id: Article.all.sample.id, tag_id: tag1.id)
 Joiner.create(article_id: Article.all.sample.id, tag_id: tag2.id)
 
 puts "seeding done"
